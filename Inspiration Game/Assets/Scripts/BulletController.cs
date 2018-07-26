c9a681fe3c2f6225ae28f13127fb07e65a239f3d
Erik Prinz <erik88d3@gmail.com>
2018-07-27 00:20:15 +0200

    Fixed Player Controller to match Aimee's stoof

--- Inspiration Game/Assets/Scripts/BulletController.cs
+++ Inspiration Game/Assets/Scripts/BulletController.cs
@@ -33,13 +33,9 @@ public class BulletController : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
-		if (other.CompareTag("Hit") && playerScript.isParrying && !isPlayerBullet)
+        if (other.CompareTag("Hit") && !isPlayerBullet)
         {
-			isPlayerBullet = true;
-			//gameObject.SetActive(false);
-			transform.up = Vector3.Reflect (transform.up, playerTrans.forward);
-			//transform.Rotate(0, 0, 125);
-			if(playerScript.isBigParry)
+            if(playerScript.isBigParry)
             {
                 transform.up = Vector3.Reflect(transform.up, playerTrans.forward);
                 isPlayerBullet = true;
--- Inspiration Game/Assets/Scripts/PlayerController.cs
+++ Inspiration Game/Assets/Scripts/PlayerController.cs
@@ -5,124 +5,123 @@ using InControl;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
-    public float playerSpeed, rotationSpeed, bigReload, smallReload, bigHitDuration, smallHitDuration;
-    public bool isBigParry, isSmallParry, isControllerConnected;
+	public float playerSpeed, rotationSpeed, bigReload, smallReload, bigHitDuration, smallHitDuration;
+	public bool isBigParry, isSmallParry, isControllerConnected;
 
-    private Vector3 dir;
-    private Transform player, playerBody, lookTarget;
-    private MeshRenderer bigHitMesh, smallHitMesh;
-    private bool canBigHit, canSmallHit;
-    private float bigTimer, smallTimer;
-    private Rigidbody playerRB;
+	private Vector3 dir;
+	private Transform player, playerBody, lookTarget;
+	private MeshRenderer bigHitMesh, smallHitMesh;
+	private bool canBigHit, canSmallHit;
+	private float bigTimer, smallTimer;
+	private Rigidbody playerRB;
 	// Use this for initialization
 	void Start () {
-        player = GameObject.Find("Player").GetComponent<Transform>();
-        playerBody = GameObject.Find("PlayerBody").GetComponent<Transform>();
-        lookTarget = GameObject.Find("LookTarget").GetComponent<Transform>();
-        bigHitMesh = GameObject.Find("BigHit").GetComponent<MeshRenderer>();
-        smallHitMesh = GameObject.Find("SmallHit").GetComponent<MeshRenderer>();
+		player = GameObject.Find("Player").GetComponent<Transform>();
+		playerBody = GameObject.Find("PlayerBody").GetComponent<Transform>();
+		lookTarget = GameObject.Find("LookTarget").GetComponent<Transform>();
+		bigHitMesh = GameObject.Find("BigHit").GetComponent<MeshRenderer>();
+		smallHitMesh = GameObject.Find("SmallHit").GetComponent<MeshRenderer>();
 
-        canBigHit = false;
-        canSmallHit = false;
+		canBigHit = false;
+		canSmallHit = false;
 
-        bigTimer = 0;
-        smallTimer = 0;
+		bigTimer = 0;
+		smallTimer = 0;
 
-        isControllerConnected = false;
-    }
-	
+		isControllerConnected = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
-        InputDevice inDevice = InputManager.ActiveDevice;
+		InputDevice inDevice = InputManager.ActiveDevice;
+		/*Debug.Log(Input.GetJoystickNames()[1]);
+		if (Input.GetJoystickNames()[0] != "Wireless Controller")
+		{
+			isControllerConnected = false;
+		}
+		else
+		{
+			isControllerConnected = true;
+		}*/
 
-        Debug.Log(Input.GetJoystickNames()[1]);
-        if (Input.GetJoystickNames()[0] != "Wireless Controller")
-        {
-            isControllerConnected = false;
-        }
-        else
-        {
-            isControllerConnected = true;
-        }
-
-        player.Translate(playerSpeed * inDevice.LeftStickX, 0, playerSpeed * inDevice.LeftStickY);
-    
-        
-
-        //rotation
-        if(Mathf.Abs(inDevice.RightStick.Vector.normalized.magnitude) > 0.05f)
-        {
-            dir.x = inDevice.RightStickX;
-            dir.z = inDevice.RightStickY;
-        }
-        else if (Mathf.Abs(inDevice.LeftStick.Vector.normalized.magnitude) > 0.05f)
-        {
-            dir.x = inDevice.LeftStickX;
-            dir.z = inDevice.LeftStickY;
-        }
-        dir.y = 0;
+		player.Translate(playerSpeed * inDevice.LeftStickX, 0, playerSpeed * inDevice.LeftStickY);
 
 
-        playerBody.rotation = Quaternion.LookRotation(dir);
 
-        //adjust reload time
-        bigTimer += Time.deltaTime;
-        if (bigTimer>=bigReload)
-        {
-            canBigHit = true;
-            bigTimer = 0;
-        }
+		//rotation
+		if(Mathf.Abs(inDevice.RightStick.Vector.normalized.magnitude) > 0.05f)
+		{
+			dir.x = inDevice.RightStickX;
+			dir.z = inDevice.RightStickY;
+		}
+		else if (Mathf.Abs(inDevice.LeftStick.Vector.normalized.magnitude) > 0.05f)
+		{
+			dir.x = inDevice.LeftStickX;
+			dir.z = inDevice.LeftStickY;
+		}
+		dir.y = 0;
 
-        smallTimer += Time.deltaTime;
-        if (smallTimer >= smallReload)
-        {
-            canSmallHit = true;
-            smallTimer = 0;
-        }
 
-        //parrying
-        if (inDevice.RightTrigger.IsPressed && canBigHit)
-        {
-            
-            StartCoroutine(ShowBigHit());
-        }
+		playerBody.rotation = Quaternion.LookRotation(dir);
 
-        if (inDevice.RightBumper.IsPressed && canSmallHit)
-        {
-            
-            StartCoroutine(ShowSmallHit());
-        }
+		//adjust reload time
+		bigTimer += Time.deltaTime;
+		if (bigTimer>=bigReload)
+		{
+			canBigHit = true;
+			bigTimer = 0;
+		}
 
-        //restart game
-        if (inDevice.MenuWasPressed)
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
+		smallTimer += Time.deltaTime;
+		if (smallTimer >= smallReload)
+		{
+			canSmallHit = true;
+			smallTimer = 0;
+		}
 
-    IEnumerator ShowBigHit()
-    {
-        canBigHit = false;
-        bigHitMesh.enabled = true;
-        isBigParry = true;
-        yield return new WaitForSecondsRealtime(bigHitDuration);
-        isBigParry = false;
-        bigHitMesh.enabled = false;
-    }
+		//parrying
+		if (inDevice.RightTrigger.IsPressed && canBigHit)
+		{
 
-    IEnumerator ShowSmallHit()
-    {
-        canSmallHit = false;
-        smallHitMesh.enabled = true;
-        isSmallParry = true;
-        yield return new WaitForSecondsRealtime(smallHitDuration);
-        isSmallParry = false;
-        smallHitMesh.enabled = false;
-    }
+			StartCoroutine(ShowBigHit());
+		}
 
-    private void FixedUpdate()
-    {
-        // keyboard controls
-        
-    }
+		if (inDevice.RightBumper.IsPressed && canSmallHit)
+		{
+
+			StartCoroutine(ShowSmallHit());
+		}
+
+		//restart game
+		if (inDevice.MenuWasPressed)
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+
+	IEnumerator ShowBigHit()
+	{
+		canBigHit = false;
+		bigHitMesh.enabled = true;
+		isBigParry = true;
+		yield return new WaitForSecondsRealtime(bigHitDuration);
+		isBigParry = false;
+		bigHitMesh.enabled = false;
+	}
+
+	IEnumerator ShowSmallHit()
+	{
+		canSmallHit = false;
+		smallHitMesh.enabled = true;
+		isSmallParry = true;
+		yield return new WaitForSecondsRealtime(smallHitDuration);
+		isSmallParry = false;
+		smallHitMesh.enabled = false;
+	}
+
+	private void FixedUpdate()
+	{
+		// keyboard controls
+
+	}
 }
